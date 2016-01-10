e0efa3ce9ff52b93572ee688d3d3e8e5e54dc15d
cstair <chrisstair@Chriss-MacBook-Air.local>
2016-01-10 12:53:23 -0500

    add min/max function

--- server.R
+++ server.R
@@ -30,17 +30,28 @@ shinyServer(
       names(weekly_returns)[names(weekly_returns)=="weekly.returns.1"] <- paste(input$stock2, "Returns")
       return(weekly_returns)
     })
+    
+    FFInput <- reactive({
+      stock_one <- stockOne()
+      stock_two <- stockTwo()
+      lwr <- pmin(stock_one, stock_two)
+      upr <- pmax(stock_one, stock_two)
+      ff_input <- cbind(stock_one, lwr, upr)
+      names(ff_input)[names(ff_input)==(paste(input$stock1, ".Close.1", sep=""))] <- "Min"
+      names(ff_input)[names(ff_input)==(paste(input$stock1, ".Close.2", sep=""))] <- "Max"
+      return(ff_input)
+    })
 
+    
     # Chart stock prices!
     output$dygraph_price <- renderDygraph({
-      dygraph(cbind(stockOne(),stockTwo()),
+      dygraph(FFInput(),
               xlab="Date",
               main=paste("Daily Closing Price of", input$stock1, "and", input$stock2, ",", input$dateRange[1], "to", input$dateRange[2]),
               group = "stockcharts") %>%
               dyAxis("x", drawGrid = TRUE) %>%
               dyAxis("y", label = paste(input$stock1, "Closing Price, USD")) %>%
               dyAxis("y2", label = paste(input$stock2, "Closing Price, USD"), independentTicks = TRUE) %>%
-              dySeries(paste(input$stock2, ".Close", sep=""), axis = "y2") %>%
               dyRangeSelector(strokeColor = "black", fillColor = "black")
     })
 
@@ -53,7 +64,7 @@ shinyServer(
               group = "stockcharts") %>%
         dyAxis("x", drawGrid = TRUE) %>%
         dyAxis("y", label = paste(input$stock1, "and", input$stock2, "Returns")) %>%
-        dyRangeSelector(strokeColor = "black", fillColor = "black")
+        dyRangeSelector(strokeColor = "black", fillColor = "black") 
     })
   }
 )
